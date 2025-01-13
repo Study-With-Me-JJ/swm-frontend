f474064a6141313a241531e3c7e4069dd7df19d7
koyoujin6759 <rhdbwls853@naver.com>
2025-01-13 19:17:56 +0900

    fix: endpoint 경로 수정

--- src/lib/api/endpoints.ts
+++ src/lib/api/endpoints.ts
@@ -1,9 +1,9 @@
 export const API_ENDPOINTS = { 
     STUDY_ROOM: {
-      LIST: '/studyroom',
-      DETAIL: (id: number | string) => `/studyroom/${id}`,
-      CREATE: '/studyroom',
-      UPDATE: (id: number | string) => `/studyroom/${id}`,
-      DELETE: (id: number | string) => `/studyroom/${id}`,
+      LIST: '/api/v1/studyroom',
+      DETAIL: (id: number | string) => `/api/v1/studyroom/${id}`,
+      CREATE: '/api/v1/studyroom',
+      UPDATE: (id: number | string) => `/api/v1/studyroom/${id}`,
+      DELETE: (id: number | string) => `/api/v1/studyroom/${id}`,
     }, 
   } as const;
