9c36406588a20634203d6235f01ebcf0d84aac16
Seoyeon Kim <kseoy0607@gmail.com>
2025-01-16 15:02:44 +0900

    feat: 헤더 버튼 링크 연결

--- src/components/HeaderUser.tsx
+++ src/components/HeaderUser.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function HeaderUser() {
     return (
         <div className='flex gap-2'>
-            <button className='text-xs font-medium text-gray-default bg-[#e9e9e9] rounded-[4px] py-[6px] px-[12px]'>회원가입</button>
-            <button className='text-xs font-medium text-gray-default bg-[#e9e9e9] rounded-[4px] py-[6px] px-[12px]'>로그인</button>
+            <button className='text-xs font-medium text-gray-default bg-[#e9e9e9] rounded-[4px] py-[6px] px-[12px]'><Link href='/join'>회원가입</Link></button>
+            <button className='text-xs font-medium text-gray-default bg-[#e9e9e9] rounded-[4px] py-[6px] px-[12px]'><Link href='/login'>로그인</Link></button>
         </div>
     )
 }   
