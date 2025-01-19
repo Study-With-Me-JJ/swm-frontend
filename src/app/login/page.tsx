d45fc2a35744955975ae64023272c695748b005c
Seoyeon Kim <kseoy0607@gmail.com>
2025-01-20 00:03:31 +0900

    feat: 로그인 페이지 개발 완료
    - 로그인 유지 기능을 제외한 나머지

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,53 @@
 'use client'
 
-import { useState } from "react"
+import jwt from 'jsonwebtoken';
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import React, { useEffect, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { InputField } from "@/components/InputField"
-import Link from "next/link"
+import { login } from "@/lib/api/auth"
+import { isApiErrorResponse } from "@/types/common"
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [idErrorText, setIdErrorText] = useState<string>('');
+  const [pwErrorText, setPwErrorText] = useState<string>('');
+  const router = useRouter();
 
   const isButtonDisabled = !email || !password;
+
+  const handleLogin = async () => {
+    try {
+      const response = await login(email, password);
+      
+      if (response.status === 200) {
+        const accessToken = response.headers['authorization'].split(' ')[1];
+        const decodedToken = jwt.decode(accessToken) as { exp: number };
+        const expirationTime = new Date(decodedToken.exp * 1000).toLocaleString('sv-SE', { timeZone: 'Asia/Seoul' }).replace('T', ' ');
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('expirationTime', expirationTime);
+        router.push('/');
+      }
+    } catch (error) {
+      if (isApiErrorResponse(error)) {
+        if (error.message === 'User not found') {
+          setIdErrorText('해당 이메일의 아이디가 존재하지 않습니다.');
+          setEmail('');
+          setPassword('');
+        } else if (error.message === 'Password mismatch') { 
+          setPwErrorText('비밀번호가 일치하지 않습니다. 다시 입력해 주세요.');
+          setPassword('');
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (password) setPwErrorText('');
+  }, [password])
 
   return (
   <div className='flex justify-center items-center' style={{ height: 'calc(100vh - 70px)' }}>
@@ -22,7 +59,8 @@ export default function Login() {
           label="아이디"
           type="email"
           placeholder="아이디를 입력해 주세요."
-          helperText="ex) studywithme@gmail.com"
+          helperText='ex) studywithme@gmail.com'
+          errorText={idErrorText}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -30,6 +68,8 @@ export default function Login() {
           label="비밀번호"
           type="password"
           placeholder="비밀번호를 입력해 주세요."
+          helperText=''
+          errorText={pwErrorText}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
@@ -44,6 +84,7 @@ export default function Login() {
         size={'lg'}
         className='w-full bg-black text-white'
         disabled={isButtonDisabled}
+        onClick={handleLogin}
         >
           로그인
         </Button>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,59 @@
+'use client'
+
 import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
 
 interface InputFieldProps {
   label: string;
   type: string;
   placeholder: string;
-  helperText?: string;
+  helperText: string;
+  errorText: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function InputField({ label, type, placeholder, helperText, value, onChange }: InputFieldProps) {
+export function InputField({ label, type, placeholder, helperText, errorText, value, onChange }: InputFieldProps) {
+        const [errorNotification, setErrorNotification] = useState<string>('');
+        const [isFocused, setIsFocused] = useState<boolean>(false);
+
+        const validateEmail = (email: string) => {
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailPattern.test(email);
+          }
+
+        const handleFocus = () => {
+            setIsFocused(true);
+            setErrorNotification('');
+        }
+
+        const handleBlur = () => {
+            setIsFocused(false);
+            if (label === '아이디' && value && !validateEmail(value)) {
+                setErrorNotification('적합하지 않은 이메일 형식입니다. 다시 입력해 주세요.');
+            }
+        }
+
+        useEffect(() => {
+            setErrorNotification(errorText);
+        }, [errorText])
+
   return (
     <div className='flex flex-col gap-2'>
       <h3 className='font-medium'>{label}</h3>
-      <Input type={type} placeholder={placeholder} value={value} onChange={onChange} />
-      {helperText && <p className='text-xs text-blue-example'>{helperText}</p>}
+      <Input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e)}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className={`${!isFocused && errorNotification && 'border-red-error'}
+        ${!isFocused && label === '비밀번호' && value && 'border-blue-example'}
+        ${!isFocused && label === '아이디' && value && validateEmail(value) && 'border-blue-example'}`} />
+
+      {label === '아이디' && !errorNotification && <p className='text-xs text-blue-example'>{helperText}</p>}
+      {!isFocused && errorNotification && <p className="text-xs text-red-error">{errorNotification}</p>}
     </div>
   );
 }
