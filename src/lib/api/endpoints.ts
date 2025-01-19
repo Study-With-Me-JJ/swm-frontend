577ffca2739d7f6844c7409b5b4a49682f02550c
Seoyeon Kim <kseoy0607@gmail.com>
2025-01-19 23:53:03 +0900

    feat: 로그인/로그아웃/회원가입 엔드포인트 추가

--- src/lib/api/endpoints.ts
+++ src/lib/api/endpoints.ts
@@ -9,5 +9,10 @@ export const API_ENDPOINTS = {
     EXTERNAL_STUDY_ROOMS: {
         LIST: '/api/v1/external-study-rooms',
     },
+    USER: {
+      LOGIN: '/api/auth/login',
+      LOGOUT: '/api/auth/logout',
+      JOIN: '/api/v1/user/custom'
+    }
   } as const;
  
