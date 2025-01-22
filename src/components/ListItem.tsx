06d5c860313c92a5b661a7ae1a495e94ffec6dec
koyoujin6759 <rhdbwls853@naver.com>
2025-01-22 19:19:54 +0900

    feat: 메인 레이아웃

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,149 +1,226 @@
-'use client'
- 
-import { Navigation } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react'; 
+'use client';
+
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+import { Swiper as SwiperType } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination'; 
-import { useEffect, useRef } from 'react';
-import Image from 'next/image'; 
+import 'swiper/css/pagination';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { NavigationOptions } from 'swiper/types';
-import { Swiper as SwiperType } from 'swiper';
-interface StudyRoom {  
-    title: string;
-    thumbnail: string;
-    locality: string;
-    likeCount: number;
-    reviewCount: number;
-    entireMinPricePerHour: number;
-    entireMaxHeadcount: number;
-    starAvg: number;
-    studyBookmarkId: number | null;
-    tags: {
-        studyRoomTagId: number;
-        tag: string;
-    }[];
+
+interface StudyRoom {
+  studyRoomId: number;
+  title: string;
+  thumbnail: string;
+  locality: string;
+  likeCount: number;
+  reviewCount: number;
+  entireMinPricePerHour: number;
+  entireMaxHeadcount: number;
+  starAvg: number;
+  studyBookmarkId: number | null;
+  tags: {
+    studyRoomTagId: number;
+    tag: string;
+  }[];
 }
 
-interface SlideItemProps {  
-    slideData: StudyRoom[];
-    useSlider?: boolean;
+interface SlideItemProps {
+  slideData: StudyRoom[];
+  useSlider?: boolean;
 }
- 
-export default function ListItem({ slideData, useSlider=false }: SlideItemProps) {
-    // console.log('slideData type:', typeof slideData, 'value:', slideData);
-  
-    useEffect(() => {
-        // Swiper 인스턴스가 생성된 후에 실행됩니다
-        const swiperElement = document.querySelector('.swiper');
-        const swiper =(swiperElement as unknown as { swiper: SwiperType })?.swiper;
-        if (swiper) {
-            swiper.update(); // Swiper 업데이트
-        }
-      }, [slideData]); // slideData가 변경될 때마다 실행
 
-    const prevRef = useRef<HTMLButtonElement>(null);
-    const nextRef = useRef<HTMLButtonElement>(null); 
+export default function ListItem({
+  slideData,
+  useSlider = false,
+}: SlideItemProps) {
+  // console.log('slideData type:', typeof slideData, 'value:', slideData);
 
-    if(!slideData) {
-        return <div>데이터가 없습니다.</div>;
+  useEffect(() => {
+    // Swiper 인스턴스가 생성된 후에 실행됩니다
+    const swiperElement = document.querySelector('.swiper');
+    const swiper = (swiperElement as unknown as { swiper: SwiperType })?.swiper;
+    if (swiper) {
+      swiper.update(); // Swiper 업데이트
     }
+  }, [slideData]); // slideData가 변경될 때마다 실행
 
-    if(slideData.length === 0) {
-        return <div>표시할 데이터가 없습니다.</div>;
-    } 
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
 
-    interface ItemContentProps {
-        item: StudyRoom;  
-    }
+  if (!slideData) {
+    return <div>데이터가 없습니다.</div>;
+  }
 
-    const ItemContent = ({item}:ItemContentProps) => {
-        return (
-            <>
-                <div>
-                    <div className="rounded-[8px] relative w-full aspect-[4/1.55] overflow-hidden">
-                        {item.thumbnail ? (
-                            // <Image src={item.thumbnail} alt={item.title} fill className="object-cover" />
-                            <Image src={'/no-image.png'} alt={item.title} fill className="object-cover" />
-                        ) : (
-                            <Image src={'/no-image.png'} alt={item.title} fill className="object-cover" />
-                        )}
-                    </div>
-                    <div className="pt-[25px]">
-                        <div>{item.title}</div>
-                        <div>{item.locality}</div>
-                    </div>
-                </div>
-            </>
-        );
-    }
-     
-    if (useSlider) {
-        return (
-            <div className="swiper-container max-w-screen-xl">
-                <Swiper  
-                    className="my-swiper"
-                    modules={[Navigation]}
-                    loop={true}  
-                    spaceBetween={20}   
-                    observer={true}
-                    observeParents={true}
-                    watchOverflow={true}
-                    navigation={{
-                        prevEl: prevRef.current,
-                        nextEl: nextRef.current,
-                    } as NavigationOptions}
-                    onSwiper={(swiper) => {
-                        setTimeout(() => {
-                            swiper.update();
-                        }, 100);
-                    }}
-                    onBeforeInit={(swiper) => {
-                        if (typeof swiper.params.navigation !== 'boolean') {
-                            const nav = swiper.params.navigation;
-                            if (nav) {
-                                nav.prevEl = prevRef.current;
-                                nav.nextEl = nextRef.current;
-                            }
-                        }
-                    }} 
-                    slidesPerView="auto" 
-                    breakpoints={{  
-                        320: {
-                            slidesPerView: 1,
-                            spaceBetween: 20
-                        },
-                        768: {
-                            slidesPerView: 2,
-                            spaceBetween: 20
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                            spaceBetween: 20
-                        }
-                    }}
-                >
-                {slideData.map((item, index) => (
-                    <SwiperSlide key={`slide-${index}`}>          
-                        <ItemContent item={item} />
-                    </SwiperSlide>  
-                ))}
-                </Swiper>
-            <div className="swiper-navigation top-4">
-                <button ref={prevRef} className="swiper-button-prev"></button>
-                <button ref={nextRef} className="swiper-button-next"></button>
-                </div>
+  if (slideData.length === 0) {
+    return <div>표시할 데이터가 없습니다.</div>;
+  }
+
+  interface ItemContentProps {
+    item: StudyRoom;
+  }
+
+  const ItemContent = ({ item }: ItemContentProps) => {
+    const handleCopy = (phoneNumber: string) => {
+      navigator.clipboard
+        .writeText(phoneNumber)
+        .then(() => {
+          alert('복사되었습니다.');
+        })
+        .catch(() => {
+          alert('복사에 실패했습니다.');
+        });
+    };
+    return (
+      <>
+        <div>
+          <div className="relative aspect-[4/1.55] w-full overflow-hidden rounded-[8px]">
+            <Link href={`/studyroom/${item.studyRoomId}`}>
+              {item.thumbnail ? (
+                // <Image src={item.thumbnail} alt={item.title} fill className="object-cover" />
+                <Image
+                  src={'/no-image.png'}
+                  alt={item.title}
+                  fill
+                  className="object-cover"
+                />
+              ) : (
+                <Image
+                  src={'/no-image.png'}
+                  alt={item.title}
+                  fill
+                  className="object-cover"
+                />
+              )}
+            </Link>
+          </div>
+          <div className="pt-[25px]">
+            <div className="flex items-center justify-between gap-1">
+              <Link
+                href={`/studyroom/${item.studyRoomId}`}
+                className="text-[20px] font-bold text-black"
+              >
+                {item.title}
+              </Link>
+              <div className="flex items-center gap-[10px]">
+                <button className="text-link-default border-link-default rounded-[4px] border px-[10px] py-[6px] text-[12px] font-semibold">
+                  홈페이지
+                </button>
+                <button className="text-link-default border-link-default rounded-[4px] border px-[10px] py-[6px] text-[12px] font-semibold">
+                  지도에서 보기
+                </button>
+              </div>
             </div>
-        ) 
-    } else {
-        return (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-screen-xl w-full">
-                {slideData.map((item, index) => (
-                    <div key={`item-${index}`}>
-                        <div>{item.title}</div>
-                    </div>
-                ))}
+            <div className="flex flex-col gap-[6px] pt-[16px]">
+              <p className="font-regular flex items-center gap-[4px] text-[14px] text-black">
+                <Image
+                  src={'/icons/icon_pin.svg'}
+                  alt="location"
+                  width={20}
+                  height={20}
+                />
+                {item.locality}
+              </p>
+              <p className="font-regular flex items-center gap-[4px] text-[14px] text-black">
+                <Image
+                  src={'/icons/icon_call.svg'}
+                  alt="call"
+                  width={20}
+                  height={20}
+                />
+                000-000-0000{' '}
+                <button onClick={() => handleCopy('000-000-0000')}>
+                  <Image
+                    src={'/icons/icon_blue_18_copy.svg'}
+                    alt="복사하기"
+                    width={20}
+                    height={20}
+                  />
+                </button>
+              </p>
             </div>
-        )
-    }
+            <div className="pt-[16px]">
+              <p className="font-regular text-[12px] text-[#828282]">
+                종로3가 5번출구로 나오셔서 낙원동 98-1로 찾아오시면됩니다. 약
+                70m
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  if (useSlider) {
+    return (
+      <div className="swiper-container max-w-screen-xl">
+        <Swiper
+          className="my-swiper"
+          modules={[Navigation]}
+          loop={true}
+          spaceBetween={20}
+          observer={true}
+          observeParents={true}
+          watchOverflow={true}
+          navigation={
+            {
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            } as NavigationOptions
+          }
+          onSwiper={(swiper) => {
+            setTimeout(() => {
+              swiper.update();
+            }, 100);
+          }}
+          onBeforeInit={(swiper) => {
+            if (typeof swiper.params.navigation !== 'boolean') {
+              const nav = swiper.params.navigation;
+              if (nav) {
+                nav.prevEl = prevRef.current;
+                nav.nextEl = nextRef.current;
+              }
+            }
+          }}
+          slidesPerView="auto"
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          {slideData.map((item, index) => (
+            <SwiperSlide key={`slide-${index}`}>
+              <ItemContent item={item} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="swiper-navigation top-4">
+          <button ref={prevRef} className="swiper-button-prev"></button>
+          <button ref={nextRef} className="swiper-button-next"></button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="grid w-full max-w-screen-xl grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {slideData.map((item, index) => (
+          <div key={`item-${index}`}>
+            <div>{item.title}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,41 @@
-import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
+import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-  	extend: {
-		fontFamily: {
-			pretendard: ['var(--font-pretendard)'],
-		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))', 
-  			primary: {
-  				'default': 'hsl(var(--primary))',  
-  			},  
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			gray: {
-				'default': '#565656',
-				'light': '#bbbbbb',
-				'disabled': '#e0e0e0'
-  			},
-			blue: {
-				'example': '#4998E9',
-			},
-			red: {
-				'error': '#E0143C'
-			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          default: 'hsl(var(--primary))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        gray: {
+          default: '#565656',
+          light: '#bbbbbb',
+          disabled: '#e0e0e0',
+        },
+        link: {
+          default: '#4998E9',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
   plugins: [animate],
 };
