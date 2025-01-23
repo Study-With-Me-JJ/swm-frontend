6bcbd6d238a75e45f50b45175a85b68a08e5cd29
koyoujin6759 <rhdbwls853@naver.com>
2025-01-23 17:30:22 +0900

    feat : 스터디룸 상세페이지 동적라우팅 추가

--- src/lib/api/endpoints.ts
+++ src/lib/api/endpoints.ts
@@ -1,7 +1,7 @@
 export const API_ENDPOINTS = { 
     STUDY_ROOM: {
       LIST: '/api/v1/studyroom',
-      DETAIL: (id: number | string) => `/api/v1/studyroom/${id}`,
+      DETAIL: (studyRoomId: number | string) => `/api/v1/studyroom/${studyRoomId}`,
       CREATE: '/api/v1/studyroom',
       UPDATE: (id: number | string) => `/api/v1/studyroom/${id}`,
       DELETE: (id: number | string) => `/api/v1/studyroom/${id}`,
