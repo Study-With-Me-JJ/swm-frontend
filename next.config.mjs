d90897bfc03e3143a96f29eeefd0bea92a991b6b
koyoujin6759 <rhdbwls853@naver.com>
2025-01-24 22:28:00 +0900

    fix : 동적라우팅 설정 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = { 
     images: {
         // Disable Next.js optimizations for images (it's not available for static sites
         // and will throw an error)
@@ -7,7 +7,8 @@ const nextConfig = {
     },
 
     // This tells Next.js to export a static build to the `out` folder
-    // output: "export", 
+    output: "export", 
+    generateStaticParams: true,
 
     // This tells Next.js to export pages as "folders with an `index.html` file inside"
     // We use this option so we can avoid having the `.html` extension at the end of the page URLs.
