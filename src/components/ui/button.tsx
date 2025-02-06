81dbdbc9b024be5745a58e977e1b72556346db52
Seoyeon Kim <kseoy0607@gmail.com>
2025-02-06 20:14:49 +0900

    feat: 버튼 기본 설정 변경

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "font-semibold text-center text-white text-[16px]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-[60px] rounded-lg px-8 text-[16px]",
+        default: "w-full h-[60px] p-[18px] rounded-lg",
+        sm: "w-36 h-[60px] p-[18px] rounded-lg",
+        lg: "rounded-lg px-8",
         icon: "h-10 w-10",
       },
     },
