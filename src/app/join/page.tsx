b1229b2c8f4804b46f8ee295ca054189dc14b2bc
Seoyeon Kim <kseoy0607@gmail.com>
2025-02-12 03:02:55 +0900

    feat: 닉네임 중복 확인 기능 구현

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -4,51 +4,77 @@ import { InputField } from "@/components/InputField";
 import { useForm, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { signupMessages } from "@/utils/signup-messages";
+import { INPUT_ERROR_MESSAGE } from "@/utils/input-error-message";
 import { Button } from "@/components/ui/button";
+import { checkNickname } from "@/lib/api/signup";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
-const schema = z.object({
-  name: z.string()
-    .min(2, signupMessages.name)
-    .regex(/^[가-힣a-zA-Z\s]+$/, signupMessages.name)
-    .optional(),
-  nickName: z.string().regex(/^[a-zA-Z0-9]+$/, signupMessages.nickName).optional(),
-  email: z.string().email(signupMessages.email).optional(),
-  authCode: z.string().optional(),
-  password: z.string().min(8, signupMessages.password).max(20, signupMessages.password).optional(),
-  passwordCheck: z.string().optional()
+const signupSchema = z.object({
+  name: z.string({required_error: INPUT_ERROR_MESSAGE.NAME})
+    .min(2, INPUT_ERROR_MESSAGE.NAME)
+    .regex(/^[가-힣a-zA-Z\s]+$/, INPUT_ERROR_MESSAGE.NAME),
+  nickName: z.string({required_error: INPUT_ERROR_MESSAGE.NICKNAME.REQUIRED}).regex(/^[a-zA-Z0-9]+$/, INPUT_ERROR_MESSAGE.NICKNAME.HELPER_TEXT),
+  email: z.string({required_error: INPUT_ERROR_MESSAGE.EMAIL.REQUIRED}).email(INPUT_ERROR_MESSAGE.EMAIL.INVALID_FORMAT),
+  authCode: z.string(),
+  password: z.string({required_error: INPUT_ERROR_MESSAGE.PASSWORD.REQUIRED}).min(8, INPUT_ERROR_MESSAGE.PASSWORD.HELPER_TEXT).max(20, INPUT_ERROR_MESSAGE.PASSWORD.HELPER_TEXT),
+  passwordCheck: z.string({required_error: INPUT_ERROR_MESSAGE.PASSWORD_CHECK})
 });
 
 
 export default function Join() {
-  const methods = useForm<z.infer<typeof schema>>({
-    resolver: zodResolver(schema),
+  const methods = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
     mode: "onBlur",
   });
+  
+  const {watch, setError, formState: {errors}} = methods;
+  const [nicknameHelperText, setNicknameHelperText] = useState<string>(INPUT_ERROR_MESSAGE.NICKNAME.HELPER_TEXT);
+
+  const nickname = watch('nickName');
+
+  const handleCheckNickname = async () => {
+    const nickname = methods.getValues('nickName');
+
+    try {
+      const isAvailable = await checkNickname(nickname);
+
+      if (isAvailable.data) {
+        setError('nickName', {message: '이미 사용 중인 닉네임입니다.'});
+      } else {
+        setNicknameHelperText('사용 가능한 닉네임입니다.');
+      }
+    } catch (error) {
+        console.error('Nickname check error:', error);
+        toast.error('닉네임 확인 중 오류가 발생했습니다. 다시 시도해 주세요.', {
+          duration: 3000,
+        })
+    }
+  }
 
     return (
       <div className='flex justify-center items-center m-16'>
-        <main className='flex flex-col w-[432px] mx-5 gap-10'>
-          <h1 className='text-2xl font-bold'>스터디 윗 미 회원가입</h1>
+        <FormProvider {...methods}>
+          <form className='flex flex-col w-[432px] mx-5 gap-10'>
+            <h1 className='text-2xl font-bold'>스터디 윗 미 회원가입</h1>
 
-          <FormProvider {...methods}>
-          <section className='flex flex-col gap-5'>
-          <InputField
+            <section className='flex flex-col gap-5'>
+            <InputField
               name="name"
               label="이름"
               type="text"
               placeholder="이름을 입력해 주세요."
-              helperText={signupMessages.name}
+              helperText={INPUT_ERROR_MESSAGE.NAME}
             />
             <InputField
               name="nickName"
-
               label="닉네임"
               type="text"
               placeholder="닉네임을 입력해 주세요."
-              helperText={signupMessages.nickName}
+              helperText={nicknameHelperText}
               buttonText="중복 확인"
-
+              onButtonClick={handleCheckNickname}
+              buttonDisabled={!nickname || !!errors.nickName}
             />
             <InputField
               name="email"
@@ -72,7 +98,7 @@ export default function Join() {
               label="비밀번호"
               type="password"
               placeholder="비밀번호를 입력해 주세요."
-              helperText={signupMessages.password}
+              helperText={INPUT_ERROR_MESSAGE.PASSWORD.HELPER_TEXT}
             />
 
 
@@ -83,10 +109,10 @@ export default function Join() {
               placeholder="비밀번호를 입력해 주세요."
             />
           </section>
-          </FormProvider>
           <Button className="bg-blue-default">가입하기</Button>
-        </main>
-      </div>
+        </form>
+      </FormProvider>
+    </div>
 
     )
   }
