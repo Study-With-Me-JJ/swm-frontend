d18480e57612c478f9852abf1d5b075db92950bc
ellen <cuttlefish77777@gmail.com>
2025-02-13 10:34:21 +0900

    docs: studyroom params 주석 추가

--- src/types/api/study-room.ts
+++ src/types/api/study-room.ts
@@ -30,15 +30,15 @@ export type StudyRoomOption =
 export interface StudyRoomListParams {
   title?: string; // 검색어
   locality?: string;
-  headCount?: number; // 인원 수
-  minPricePerHour?: number; // 최소 시간당 가격 -> 최소 >= DB에 저장된 값 <= 최대
+  headCount?: number; // 인원 수:50명까지 제한 / condition --> entireMaxHeadcount >= headCount => return item
+  minPricePerHour?: number; // 최소 시간당 가격: 최소 >= DB에 저장된 값 <= 최대
   maxPricePerHour?: number; // 최대 시간당 가격
   options?: StudyRoomOption[]; // 옵션들
   sortCriteria?: SortCriteria; // 정렬 기준
   userLatitude?: number; // 유저 위도
   userLongitude?: number; // 유저 경도
   lastStudyRoomId?: number; // 마지막 스터디룸 ID (커서)
-  lastSortValue?: number; // 마지막 정렬 값 (커서)
+  lastSortValue?: number; // 마지막 정렬 값 (커서): LIKE, REVIEW, PRICE_ASC, PRICE_DESC
   lastLatitudeValue?: number; // 마지막 위도 값 (커서)
   lastLongitudeValue?: number; // 마지막 경도 값 (커서)
   lastAverageRatingValue?: number; // 마지막 평점 값 (커서)
