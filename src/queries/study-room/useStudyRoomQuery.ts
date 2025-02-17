574358413bd862f5376f94256c26a6a05ad7a3bf
ellen <cuttlefish77777@gmail.com>
2025-02-17 18:32:43 +0900

    style: code 코멧팅

--- src/queries/study-room/useStudyRoomQuery.ts
+++ src/queries/study-room/useStudyRoomQuery.ts
@@ -1,12 +1,20 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
 import { StudyRoomService } from '@/lib/api/services';
-import { SortCriteria, StudyRoomListParams, StudyRoomListRes } from '@/types/api';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { devLog } from '@/utils/dev-log';
+import {
+  SortCriteria,
+  StudyRoomListParams,
+  StudyRoomListRes,
+} from '@/types/api';
 
 export const studyRoomQueryKey = {
-  list: (filters: Partial<StudyRoomListParams>) => ['studyRoomList', filters] as const
+  list: (filters: Partial<StudyRoomListParams>) =>
+    ['studyRoomList', filters] as const,
 };
-export const getSortValueByCriteria = (sortCriteria: Exclude<SortCriteria, 'STARS'> | undefined, lastItem: StudyRoomListRes) => {
+export const getSortValueByCriteria = (
+  sortCriteria: Exclude<SortCriteria, 'STARS'> | undefined,
+  lastItem: StudyRoomListRes,
+) => {
   const sortValueMap: Record<Exclude<SortCriteria, 'STAR'>, number> = {
     [SortCriteria.LIKE]: lastItem.likeCount,
     [SortCriteria.REVIEW]: lastItem.reviewCount,
@@ -18,33 +26,48 @@ export const getSortValueByCriteria = (sortCriteria: Exclude<SortCriteria, 'STAR
 };
 
 export const useStudyRoomQuery = (filters: Partial<StudyRoomListParams>) => {
-  return useInfiniteQuery({
+  return useSuspenseInfiniteQuery({
     queryKey: studyRoomQueryKey.list(filters),
-    queryFn: async ({ pageParam }: { pageParam: Pick<StudyRoomListParams, 'lastStudyRoomId' | 'lastSortValue' | 'lastAverageRatingValue' | 'lastLatitudeValue' | 'lastLongitudeValue'> | null }) => {
-      const response = await StudyRoomService.getStudyRoomList({ 
+    queryFn: async ({
+      pageParam,
+    }: {
+      pageParam: Pick<
+        StudyRoomListParams,
+        | 'lastStudyRoomId'
+        | 'lastSortValue'
+        | 'lastAverageRatingValue'
+        | 'lastLatitudeValue'
+        | 'lastLongitudeValue'
+      > | null;
+    }) => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      const response = await StudyRoomService.getStudyRoomList({
         queryParams: {
-          ...filters, 
+          ...filters,
           lastStudyRoomId: pageParam?.lastStudyRoomId,
           lastSortValue: pageParam?.lastSortValue,
           lastAverageRatingValue: pageParam?.lastAverageRatingValue,
           lastLatitudeValue: pageParam?.lastLatitudeValue,
           lastLongitudeValue: pageParam?.lastLongitudeValue,
-        }
+        },
       });
       return response;
     },
     initialPageParam: null,
     getNextPageParam: (lastPage, TQueryFnData) => {
       devLog.info('TQueryFnData', TQueryFnData);
-      
+
       if (lastPage.data.hasNext && lastPage.data.data.length > 0) {
         const lastItem = lastPage.data.data[lastPage.data.data.length - 1];
         const { sortCriteria } = filters;
-        const lastSortValue = getSortValueByCriteria(sortCriteria as Exclude<SortCriteria, 'STARS'>  ?? undefined, lastItem);
+        const lastSortValue = getSortValueByCriteria(
+          (sortCriteria as Exclude<SortCriteria, 'STARS'>) ?? undefined,
+          lastItem,
+        );
         return {
           lastStudyRoomId: lastItem.studyRoomId,
           lastSortValue: lastSortValue,
-          lastAverageRatingValue: lastItem.starAvg, 
+          lastAverageRatingValue: lastItem.starAvg,
           lastLatitudeValue: lastItem.coordinates.latitude,
           lastLongitudeValue: lastItem.coordinates.longitude,
         };
@@ -52,4 +75,4 @@ export const useStudyRoomQuery = (filters: Partial<StudyRoomListParams>) => {
       return null;
     },
   });
-}; 
+};
