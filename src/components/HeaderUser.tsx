81c7d0b92618273c430af3fc0c901bde329a5246
koyoujin6759 <rhdbwls853@naver.com>
2025-02-21 08:43:50 +0900

    fix: 헤더 유저정보 임시주석

--- src/components/HeaderUser.tsx
+++ src/components/HeaderUser.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
-import Image from 'next/image';
+// import { useQuery } from '@tanstack/react-query';
+// import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
