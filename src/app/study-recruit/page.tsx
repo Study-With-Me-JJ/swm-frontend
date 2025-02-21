b32cc424bb651defaee56ea4a052ac0ddf03f0d0
koyoujin6759 <rhdbwls853@naver.com>
2025-02-21 09:05:55 +0900

    fix: 빌드 에러 해결중..

--- src/app/study-recruit/page.tsx
+++ src/app/study-recruit/page.tsx
@@ -1,93 +1,136 @@
-'use client'
+'use client';
 
-import { useState, useEffect } from 'react';
+import { getStudy } from '@/lib/api/study/getStudy';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import {
+  RecruitmentPositionTitle,
+  SearchStudyParams,
+  SortCriteria,
+  Study,
+  StudyCategory,
+  StudyStatus,
+} from '@/types/api/study';
 import StudyItem from '@/components/StudyItem';
 import CategoryFilter from '@/components/filters/CategoryFilter';
 import PositionFilter from '@/components/filters/PositionFilter';
+import SortFilter from '@/components/filters/SortFilter';
 import StatusFilter from '@/components/filters/StatusFilter';
-import SortFilter from '@/components/filters/SortFilter'; 
-import Image from 'next/image'; 
-import { useInfiniteQuery } from '@tanstack/react-query';
-import {  Study, StudyCategory, StudyStatus, RecruitmentPositionTitle, SortCriteria, SearchStudyParams } from '@/types/api/study'; 
-import { getStudy } from '@/lib/api/study/getStudy';
-import Loading from '@/components/ui/Loading'; 
-import { useRef } from 'react';
+import Loadingbar from '@/components/ui/Loadingbar';
 
 export default function StudyRecruit() {
-  const [selectCategory,setSelectCategory] = useState<string | string[]>('ALL');
-  const [selectPosition,setSelectPosition] = useState<string | string[]>('ALL');
-  const [selectStatus,setSelectStatus] = useState<string | string[]>('ALL');
-  const [selectSort,setSelectSort] = useState<string | string[]>(SortCriteria.NEWEST);
+  const [selectCategory, setSelectCategory] = useState<string | string[]>(
+    'ALL',
+  );
+  const [selectPosition, setSelectPosition] = useState<string | string[]>(
+    'ALL',
+  );
+  const [selectStatus, setSelectStatus] = useState<string | string[]>('ALL');
+  const [selectSort, setSelectSort] = useState<string | string[]>(
+    SortCriteria.NEWEST,
+  );
 
   const [openSelectId, setOpenSelectId] = useState<string | null>(null);
 
-  
-  const { data: study, isLoading, error, fetchNextPage, hasNextPage } = useInfiniteQuery({
-    queryKey: ['study', selectSort,selectCategory, selectPosition, selectStatus],
-    initialPageParam: { lastStudyId: 0, lastSortValue: 0 },  
+  const {
+    data: study,
+    isLoading,
+    error,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: [
+      'study',
+      selectSort,
+      selectCategory,
+      selectPosition,
+      selectStatus,
+    ],
+    initialPageParam: { lastStudyId: 0, lastSortValue: 0 },
     queryFn: async ({ pageParam = { lastStudyId: 0, lastSortValue: 0 } }) => {
       const params: SearchStudyParams = {
         sortCriteria: selectSort as SortCriteria,
-        ...(selectCategory !== 'ALL' && { category: selectCategory as StudyCategory }),
+        ...(selectCategory !== 'ALL' && {
+          category: selectCategory as StudyCategory,
+        }),
         ...(selectStatus !== 'ALL' && { status: selectStatus as StudyStatus }),
-        ...(selectPosition !== 'ALL' && Array.isArray(selectPosition) && selectPosition.length > 0 && { 
-          recruitmentPositionTitleList: selectPosition as RecruitmentPositionTitle[]
-        }),
-        ...(selectPosition !== 'ALL' && !Array.isArray(selectPosition) && { 
-          recruitmentPositionTitleList: [selectPosition as RecruitmentPositionTitle]
-        }),
+        ...(selectPosition !== 'ALL' &&
+          Array.isArray(selectPosition) &&
+          selectPosition.length > 0 && {
+            recruitmentPositionTitleList:
+              selectPosition as RecruitmentPositionTitle[],
+          }),
+        ...(selectPosition !== 'ALL' &&
+          !Array.isArray(selectPosition) && {
+            recruitmentPositionTitleList: [
+              selectPosition as RecruitmentPositionTitle,
+            ],
+          }),
         ...(pageParam?.lastStudyId && { lastStudyId: pageParam.lastStudyId }),
-        ...(pageParam?.lastSortValue && { lastSortValue: pageParam.lastSortValue })
-      };  
+        ...(pageParam?.lastSortValue && {
+          lastSortValue: pageParam.lastSortValue,
+        }),
+      };
 
       return await getStudy(params);
     },
     getNextPageParam: (lastPage) => {
       if (!lastPage.data.hasNext) return undefined;
-  
+
       const lastItem = lastPage.data.data[lastPage.data.data.length - 1];
       return {
         lastStudyId: lastItem.studyId,
-        lastSortValue: selectSort === SortCriteria.LIKE 
-          ? lastItem.likeCount
-          : selectSort === SortCriteria.COMMENT
-            ? lastItem.commentCount
-            : lastItem.studyId  
+        lastSortValue:
+          selectSort === SortCriteria.LIKE
+            ? lastItem.likeCount
+            : selectSort === SortCriteria.COMMENT
+              ? lastItem.commentCount
+              : lastItem.studyId,
       };
-    }
+    },
   });
 
   const observerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
-      entries => {
+      (entries) => {
         if (entries[0].isIntersecting && hasNextPage && !isLoading) {
           fetchNextPage();
         }
       },
-      { threshold: 1.0 }
+      { threshold: 1.0 },
     );
-  
+
     if (observerRef.current) {
       observer.observe(observerRef.current);
     }
-  
+
     return () => {
       observer.disconnect();
     };
   }, [fetchNextPage, hasNextPage, isLoading]);
 
   if (isLoading) {
-    return <div className='flex justify-center items-center max-w-screen-xl mx-auto h-screen'><Loading /></div>;
+    return (
+      <div className="mx-auto flex h-screen max-w-screen-xl items-center justify-center">
+        <Loadingbar />
+      </div>
+    );
   }
 
   if (error) {
-    return <div className='flex justify-center items-center max-w-screen-xl mx-auto h-screen'>에러가 발생했습니다.</div>;
-  } 
+    return (
+      <div className="mx-auto flex h-screen max-w-screen-xl items-center justify-center">
+        에러가 발생했습니다.
+      </div>
+    );
+  }
 
   const handleCategoryChange = (value: string | string[]) => {
-    setSelectCategory(value || 'ALL'); 
+    setSelectCategory(value || 'ALL');
   };
 
   const handlePositionChange = (value: string | string[]) => {
@@ -97,25 +140,27 @@ export default function StudyRecruit() {
         setSelectPosition('ALL');
         return;
       }
-      
-      const positions = value.filter(v => v !== 'ALL').map(v => v as RecruitmentPositionTitle);
+
+      const positions = value
+        .filter((v) => v !== 'ALL')
+        .map((v) => v as RecruitmentPositionTitle);
       setSelectPosition(positions);
       return;
     }
-    setSelectPosition(value || 'ALL'); 
+    setSelectPosition(value || 'ALL');
   };
 
   const handleStatusChange = (value: string | string[]) => {
-    setSelectStatus(value || 'ALL'); 
+    setSelectStatus(value || 'ALL');
   };
 
   const handleSortChange = (value: string | string[]) => {
-    setSelectSort(value || SortCriteria.NEWEST); 
+    setSelectSort(value || SortCriteria.NEWEST);
   };
 
   const categoryLabels: Record<StudyCategory, string> = {
     [StudyCategory.ALGORITHM]: '알고리즘',
-    [StudyCategory.DEVELOPMENT]: '개발', 
+    [StudyCategory.DEVELOPMENT]: '개발',
   };
 
   const categoryOptions = [
@@ -123,10 +168,10 @@ export default function StudyRecruit() {
     ...Object.entries(StudyCategory).map(([, value], index) => ({
       id: index + 1,
       value,
-      label: categoryLabels[value as StudyCategory]
-    }))
+      label: categoryLabels[value as StudyCategory],
+    })),
   ];
-  
+
   const positionLabels: Record<RecruitmentPositionTitle, string> = {
     [RecruitmentPositionTitle.BACKEND]: '백엔드',
     [RecruitmentPositionTitle.FRONTEND]: '프론트엔드',
@@ -137,26 +182,28 @@ export default function StudyRecruit() {
     { id: 0, value: 'ALL', label: '직무 전체' },
     ...Object.entries(RecruitmentPositionTitle).map(([, value], index) => ({
       id: index + 1,
-      value, 
-      label: positionLabels[value as RecruitmentPositionTitle]
-    }))
-  ]  
+      value,
+      label: positionLabels[value as RecruitmentPositionTitle],
+    })),
+  ];
 
   const SORT_OPTIONS = {
     [SortCriteria.NEWEST]: '최신순',
     [SortCriteria.LIKE]: '좋아요순',
-    [SortCriteria.COMMENT]: '댓글순'
+    [SortCriteria.COMMENT]: '댓글순',
   } as const;
 
-  const sortOptions = Object.entries(SORT_OPTIONS).map(([value, label], index) => ({
-    id: index,
-    value,
-    label
-  }));
+  const sortOptions = Object.entries(SORT_OPTIONS).map(
+    ([value, label], index) => ({
+      id: index,
+      value,
+      label,
+    }),
+  );
 
   const STATUS_OPTIONS = {
     [StudyStatus.ACTIVE]: '모집중',
-    [StudyStatus.INACTIVE]: '모집마감'
+    [StudyStatus.INACTIVE]: '모집마감',
   } as const;
 
   const statusOptions = [
@@ -164,45 +211,97 @@ export default function StudyRecruit() {
     ...Object.entries(STATUS_OPTIONS).map(([value, label], index) => ({
       id: index + 1,
       value,
-      label
-    }))
+      label,
+    })),
   ];
 
   return (
-    <section className='pt-10 pb-[110px] max-w-screen-xl px-5 xl:px-0  mx-auto'>
-      <h2 className='text-2xl font-semibold mb-[34px]'>스터디 모집</h2>
-      <div className='flex justify-between items-end mb-[34px]'>
-        <div className='flex items-center gap-5 justify-start'>
-          <CategoryFilter type='default' onChange={handleCategoryChange} defaultValue={selectCategory} options={categoryOptions} isOpen={openSelectId === 'select1'}
-          onToggle={() => setOpenSelectId(openSelectId === 'select1' ? null : 'select1')}/>
-          <PositionFilter type='button' onChange={handlePositionChange} defaultValue={selectPosition} options={positionOptions} isOpen={openSelectId === 'select2'}
-          onToggle={() => setOpenSelectId(openSelectId === 'select2' ? null : 'select2')} />
-          <StatusFilter type='default' onChange={handleStatusChange} defaultValue={selectStatus} options={statusOptions} isOpen={openSelectId === 'select3'}
-          onToggle={() => setOpenSelectId(openSelectId === 'select3' ? null : 'select3')}  />
+    <section className="mx-auto max-w-screen-xl px-5 pb-[110px] pt-10 xl:px-0">
+      <h2 className="mb-[34px] text-2xl font-semibold">스터디 모집</h2>
+      <div className="mb-[34px] flex items-end justify-between">
+        <div className="flex items-center justify-start gap-5">
+          <CategoryFilter
+            type="default"
+            onChange={handleCategoryChange}
+            defaultValue={selectCategory}
+            options={categoryOptions}
+            isOpen={openSelectId === 'select1'}
+            onToggle={() =>
+              setOpenSelectId(openSelectId === 'select1' ? null : 'select1')
+            }
+          />
+          <PositionFilter
+            type="button"
+            onChange={handlePositionChange}
+            defaultValue={selectPosition}
+            options={positionOptions}
+            isOpen={openSelectId === 'select2'}
+            onToggle={() =>
+              setOpenSelectId(openSelectId === 'select2' ? null : 'select2')
+            }
+          />
+          <StatusFilter
+            type="default"
+            onChange={handleStatusChange}
+            defaultValue={selectStatus}
+            options={statusOptions}
+            isOpen={openSelectId === 'select3'}
+            onToggle={() =>
+              setOpenSelectId(openSelectId === 'select3' ? null : 'select3')
+            }
+          />
         </div>
-        <div className='flex items-center gap-1'>
+        <div className="flex items-center gap-1">
           {/* <TagFilter type='button' onChange={handleTagChange} defaultValue={selectTag} options={dummyTags.map((tag)=> ({id: tag.id,value: tag.value, label: tag.label}))} isOpen={openSelectId === 'select4'} onToggle={() => setOpenSelectId(openSelectId === 'select4' ? null : 'select4')} filterName='태그' /> */}
-          <SortFilter type='default' onChange={handleSortChange} defaultValue={selectSort} options={sortOptions.map((sort)=> ({id: sort.id,value: sort.value, label: sort.label}))} isOpen={openSelectId === 'select4'} onToggle={() => setOpenSelectId(openSelectId === 'select4' ? null : 'select4')} filterName='최신순' />
+          <SortFilter
+            type="default"
+            onChange={handleSortChange}
+            defaultValue={selectSort}
+            options={sortOptions.map((sort) => ({
+              id: sort.id,
+              value: sort.value,
+              label: sort.label,
+            }))}
+            isOpen={openSelectId === 'select4'}
+            onToggle={() =>
+              setOpenSelectId(openSelectId === 'select4' ? null : 'select4')
+            }
+            filterName="최신순"
+          />
         </div>
       </div>
-      {!study?.pages || study.pages[0].data.data.length === 0? (
-        <div className='h-[300px] flex justify-center items-center'>
-          <div className='flex flex-col items-center justify-center w-full max-w-[480px] bg-[#f9f9f9] rounded-[8px] py-[40px]'>
-            <Image src='/icons/icon_no_result.svg' alt='search' width={65} height={65} />
-            <h3 className='mt-[30px] text-[20px] font-semibold text-black'>스터디를 찾지 못했습니다.</h3>
-            <p className='mt-[12px] text-gray-default text-sm text-regular'>검색 결과가 없습니다.</p>
+      {!study?.pages || study.pages[0].data.data.length === 0 ? (
+        <div className="flex h-[300px] items-center justify-center">
+          <div className="flex w-full max-w-[480px] flex-col items-center justify-center rounded-[8px] bg-[#f9f9f9] py-[40px]">
+            <Image
+              src="/icons/icon_no_result.svg"
+              alt="search"
+              width={65}
+              height={65}
+            />
+            <h3 className="mt-[30px] text-[20px] font-semibold text-black">
+              스터디를 찾지 못했습니다.
+            </h3>
+            <p className="text-regular mt-[12px] text-sm text-gray-default">
+              검색 결과가 없습니다.
+            </p>
           </div>
         </div>
       ) : (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-[26px] max-w-screen-xl w-full'> 
-        {study.pages.map((page) => {  
-          return page.data.data.map((item) => { 
-            return <StudyItem key={`${item.studyId}-${item.title}`} data={item as Study}/>;
-          });
-        })}
-      </div>
+        <div className="grid w-full max-w-screen-xl grid-cols-1 gap-x-5 gap-y-[26px] md:grid-cols-2 lg:grid-cols-3">
+          {study.pages.map((page) => {
+            return page.data.data.map((item) => {
+              return (
+                <StudyItem
+                  key={`${item.studyId}-${item.title}`}
+                  data={item as Study}
+                />
+              );
+            });
+          })}
+        </div>
       )}
-      <div ref={observerRef} className="h-4" /> 
+      <div ref={observerRef} className="h-4" />
     </section>
-  )
+  );
 }
--- src/components/StudyRoomSection.tsx
+++ src/components/StudyRoomSection.tsx
@@ -1,39 +1,61 @@
-"use client"
+'use client';
 
-import StudyroomItem from "./StudyroomItem";
-import Link from "next/link";
-import Image from "next/image";
-import { getStudyRoom } from "@/lib/api/study/getStudyRoom";
-import { useQuery } from "@tanstack/react-query";
-import Loading from "@/components/ui/Loading";
+import { getStudyRoom } from '@/lib/api/study/getStudyRoom';
+import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
+import Link from 'next/link';
+import StudyroomItem from './StudyroomItem';
+import Loadingbar from '@/components/ui/Loadingbar';
 
 export default function StudyRoomSection() {
-    
-    const { data: studyRooms, isLoading, error } = useQuery({
-        queryKey: ['studyRooms'],
-        queryFn: getStudyRoom,
-        staleTime: 1000 * 60 * 60,  
-        gcTime: 1000 * 60 * 60 * 24,  
-    });
-    const studyRoomsData = studyRooms?.data;
-    // console.log('studyRoomsData type:', typeof studyRoomsData, 'value:', studyRoomsData);
-    if (error) return <div>에러가 발생했습니다</div>;
+  const {
+    data: studyRooms,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['studyRooms'],
+    queryFn: getStudyRoom,
+    staleTime: 1000 * 60 * 60,
+    gcTime: 1000 * 60 * 60 * 24,
+  });
+  const studyRoomsData = studyRooms?.data;
+  // console.log('studyRoomsData type:', typeof studyRoomsData, 'value:', studyRoomsData);
+  if (error) return <div>에러가 발생했습니다</div>;
 
-    return <>
-        <article className='relative'>
-          <div className='flex items-center justify-between mb-[40px]'>
-            <div className='flex items-center gap-[20px] '>
-              <h3 className='text-left text-2xl font-semibold text-black'>요즘 뜨는 스터디룸</h3>
-              <Link href='/studyroom' className='flex items-center gap-[6px] text-base font-semibold text-gray-light'>전체보기 <Image src="/icons/icon_gry_18_back.svg" alt="arrow-right" width={14} height={14}/></Link>
-            </div>
+  return (
+    <>
+      <article className="relative">
+        <div className="mb-[40px] flex items-center justify-between">
+          <div className="flex items-center gap-[20px]">
+            <h3 className="text-left text-2xl font-semibold text-black">
+              요즘 뜨는 스터디룸
+            </h3>
+            <Link
+              href="/studyroom"
+              className="flex items-center gap-[6px] text-base font-semibold text-gray-light"
+            >
+              전체보기{' '}
+              <Image
+                src="/icons/icon_gry_18_back.svg"
+                alt="arrow-right"
+                width={14}
+                height={14}
+              />
+            </Link>
           </div>
-          {isLoading ? (
-            <div className='flex justify-center items-center h-[300px]'><Loading /></div>
-          ) : studyRoomsData && (
-            <div className='flex items-center gap-[20px] max-w-screen-xl w-full'>
-              <StudyroomItem slideData={studyRoomsData.data} useSlider={true}/>
+        </div>
+        {isLoading ? (
+          <div className="flex h-[300px] items-center justify-center">
+            <Loadingbar />
+          </div>
+        ) : (
+          studyRoomsData && (
+            <div className="flex w-full max-w-screen-xl items-center gap-[20px]">
+              <StudyroomItem slideData={studyRoomsData.data} useSlider={true} />
             </div>
-          )}
-        </article>
+          )
+        )}
+      </article>
     </>
+  );
 }
